a78600aab73e6c2643617accbadd02f872b1492f
Cong Nguyen <n_cong@stagegroup.jp>
2020-05-03 22:30:57 +0700

    bloc state

---
+++ lib/state_experiments/src/bloc/bloc_cart_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/state_experiments/common/models/cart_item.dart';
+import 'package:flutter_app/state_experiments/common/widgets/cart_page.dart';
+import 'package:flutter_app/state_experiments/src/bloc/cart_provider.dart';
+
+class BlocCartPage extends StatelessWidget {
+  BlocCartPage();
+
+  static const routeName = "/cart";
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = CartProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: StreamBuilder<List<CartItem>>(
+        stream: cart.items,
+        builder: (context, snapshot) {
+          if (snapshot.data == null || snapshot.data.isEmpty) {
+            return Center(
+              child: Text(
+                'Empty',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            );
+          }
+
+          return ListView(
+            children:
+                snapshot.data.map((item) => ItemTile(item: item)).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
