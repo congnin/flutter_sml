c4331553dab8f80cac9cc947ec312428435b48e9
Cong Nguyen <n_cong@stagegroup.jp>
2020-05-18 19:25:59 +0700

    .

---
+++ lib/scratch_ui/instagram/screens/feed_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/scratch_ui/instagram/models/post_model.dart';
+import 'package:flutter_app/scratch_ui/instagram/models/user_model.dart';
+import 'package:flutter_app/scratch_ui/instagram/services/database_service.dart';
+import 'package:flutter_app/scratch_ui/instagram/widgets/post_view.dart';
+
+class FeedScreen extends StatefulWidget {
+  static final String id = 'feed_screen';
+  final String currentUserId;
+
+  FeedScreen({this.currentUserId});
+
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  List<Post> _posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _setupFeed();
+  }
+
+  _setupFeed() async {
+    List<Post> posts = await DatabaseService.getFeedPosts(widget.currentUserId);
+    setState(() {
+      _posts = posts;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Instagram',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Billabong',
+            fontSize: 35.0,
+          ),
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => _setupFeed(),
+        child: ListView.builder(
+          itemCount: _posts.length,
+          itemBuilder: (BuildContext context, int index) {
+            Post post = _posts[index];
+            return FutureBuilder(
+              future: DatabaseService.getUserWithId(post.authorId),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData) {
+                  return SizedBox.shrink();
+                }
+                User author = snapshot.data;
+                return PostView(
+                  currentUserId: widget.currentUserId,
+                  post: post,
+                  author: author,
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
