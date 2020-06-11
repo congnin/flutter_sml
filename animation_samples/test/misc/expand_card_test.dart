07e5b0be973fb8ccd16e435084c946b9d4b8aea6
Cong Nguyen <n_cong@stagegroup.jp>
2020-06-11 12:08:23 +0700

    .

---
+++ animation_samples/test/misc/expand_card_test.dart
@@ -0,0 +1,45 @@
+import 'package:animation_samples/misc/expand_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Widget createExpandCardScreen() => MaterialApp(
+      home: ExpandCardDemo(),
+    );
+
+void main() {
+  group('ExpandCard Tests', () {
+    testWidgets('ExpandCard changes size on tap', (tester) async {
+      await tester.pumpWidget(createExpandCardScreen());
+
+      // Get the initial size of ExpandCard.
+      var initialSize = tester.getSize(find.byType(ExpandCard));
+
+      // Tap on the ExpandCard.
+      await tester.tap(find.byType(ExpandCard));
+      await tester.pumpAndSettle();
+
+      // The size of ExpandCard must change once tapped.
+      // The initialSize should be less than current ExpandCard size.
+      expect(
+        initialSize,
+        lessThan(tester.getSize(find.byType(ExpandCard))),
+      );
+    });
+
+    testWidgets('ExpandCard changes image on tap', (tester) async {
+      await tester.pumpWidget(createExpandCardScreen());
+
+      var initialImage = tester.widget(find.byType(Image).last);
+
+      // Tap on ExpandCard.
+      await tester.tap(find.byType(ExpandCard));
+      await tester.pumpAndSettle();
+
+      // Once tapped, the image should change.
+      expect(
+        initialImage,
+        isNot(equals(tester.widget(find.byType(Image).last))),
+      );
+    });
+  });
+}
