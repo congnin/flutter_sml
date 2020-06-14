7bb6b1031e09d8d6c45034ff7a1d108f94743aa4
Cong Nguyen <n_cong@stagegroup.jp>
2020-06-14 09:34:04 +0700

    almost done cookbook except test part

---
+++ cook_book/lib/networking/authenticated_request.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http.get(
+    'https://jsonplaceholder.typicode.com/albums/1',
+    headers: {HttpHeaders.authorizationHeader: "Basic your_api_token_here"},
+  );
+  final responseJson = json.decode(response.body);
+
+  return Album.fromJson(responseJson);
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({this.userId, this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
