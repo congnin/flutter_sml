4b9504a8ddf328be0564442d17b1623aa9e3669b
Cong Nguyen <n_cong@stagegroup.jp>
2020-07-28 08:38:45 +0700

    animated progress circle

---
+++ flutter_book_examples/animated_container/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool b = false;
+
+  void _changeMode() {
+    setState(() {
+      b = !b;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            AnimatedContainer(
+                color: b ? Colors.tealAccent : Colors.blueAccent,
+                height: b ? 300.0 : 150.0,
+                duration: Duration(seconds: 1),
+                child: Center(
+                    child: Text('Top',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w200,
+                        ),
+                        textAlign: TextAlign.center))),
+            AnimatedContainer(
+              color: b ? Colors.redAccent : Colors.orangeAccent,
+              height: b ? 150.0 : 300.0,
+              duration: Duration(seconds: 1),
+              child: Center(
+                child: Text(
+                  'Bottom',
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w200),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeMode,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ flutter_book_examples/animated_progress_circle/android/app/src/main/kotlin/com/example/animated_progress_circle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_progress_circle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
