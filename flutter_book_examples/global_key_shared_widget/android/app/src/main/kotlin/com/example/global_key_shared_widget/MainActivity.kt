71cb5d4b41023a4f78710538ce3dce4caecf91a9
Cong Nguyen <n_cong@stagegroup.jp>
2020-07-30 06:47:28 +0700

    http

---
+++ flutter_book_examples/global_key_shared_widget/android/app/src/main/kotlin/com/example/global_key_shared_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.global_key_shared_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_book_examples/stateless_widget_rebuild/android/app/src/main/kotlin/com/example/stateless_widget_rebuild/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stateless_widget_rebuild
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
